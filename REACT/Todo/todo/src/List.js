dcfc15cfb940aa5ad97e6c8394329306b6f852b7
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-01-15 18:55:43 -0300

    atlz

---
+++ REACT/Todo/todo/src/List.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function List(props){
+
+    return (
+        <ul>
+          {props.items.map(item => <li>{item}</li>)}
+        </ul>) 
+}
+
+export default List
--- REACT/Todo/todo/src/Todo.js
+++ REACT/Todo/todo/src/Todo.js
@@ -1,34 +1,25 @@
 import React, { useState } from "react";
-import './Todo.css'
-function Todo(){
+import List from './List';
+import TodoForm from "./TodoForm";
+import './Todo.css';
 
-    const [text, setText] = useState("");
+function Todo() {
+
     const [items, setItems] = useState([]);
 
-    function handleChange(event) {
-        let t = event.target.value;
-        setText(t);
-    }
-
-    function addItem(event) {
-        event.preventDefault();
-        setItems([...items, text])
-        setText("");
-    }
+   function onAddItem (item){
+    setItems([...items, item])
+   }
+   
 
     return (<div className="container">
         <h1>Todo</h1>
-        <form>
-            <input onChange={handleChange} type="text" value={text}></input>
-            <button onClick={addItem}>Add</button>
-        </form>
-        <ul>
-          {items.map(item => <li>{item}</li>)}
-        </ul>
-        </div>)
+        <TodoForm onAddItem={onAddItem}></TodoForm>
+        
+        <List items={items}></List>
+    </div>)
+
 }
-
-
 
 
 
