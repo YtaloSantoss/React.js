923a8ea9893db476e5b56b7a6e2f5b070d4b88fc
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-01-16 15:41:51 -0300

    atlz

--- REACT/Todo/todo/src/components/List.js
+++ REACT/Todo/todo/src/components/List.js
@@ -16,8 +16,10 @@ function List(props){
     return (
         <ul>
           {props.items.map(item => <li className={item.done ? "done item" : "item"} key={item.id}>{item.text}
+          <div>
           <button onClick={()=>{props.onDone(item)}}><DoneImg done={item.done}></DoneImg></button>
           <button  onClick={()=>{props.onItemDeleted(item)}}><img alt="delete" src="./bin-1.png"></img></button>
+          </div>
           </li>)}
         </ul>) 
 }
