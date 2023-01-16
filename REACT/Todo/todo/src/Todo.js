af9e32703f7043f3f79965b4553c95fdc11b85bb
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-01-16 15:30:16 -0300

    atlz

--- REACT/Todo/todo/src/Todo.js
+++ REACT/Todo/todo/src/Todo.js
@@ -1,22 +1,46 @@
 import React, { useState } from "react";
-import List from './List';
-import TodoForm from "./TodoForm";
+import List from './components/List';
+import Item from './components/Item';
+import TodoForm from "./components/TodoForm";
 import './Todo.css';
 
 function Todo() {
 
     const [items, setItems] = useState([]);
 
-   function onAddItem (item){
-    setItems([...items, item])
+   function onAddItem (text) {
+
+    let it = new Item(text);
+
+    setItems([...items, it])
    }
-   
+    
+   function onItemDeleted(item){
+    
+    let filteredItems = items.filter(it=>it.id != item.id)
+
+    setItems(filteredItems)
+  }
+
+  function onDone(item){
+
+    let updatedItems = items.map(it=>{
+        if(it.id === item.id){
+            it.done = !it.done;
+        }
+        return it;
+    })
+
+    setItems(updatedItems);
+  }
+
+
 
     return (<div className="container">
         <h1>Todo</h1>
         <TodoForm onAddItem={onAddItem}></TodoForm>
         
-        <List items={items}></List>
+        <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
     </div>)
 
 }
---
+++ REACT/Todo/todo/src/components/List.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+ function DoneImg(props){
+
+    if(props.done){
+      return (<img alt="check" src="./check-1.png"></img>)
+    } else {
+      return (<img alt="off" src="./off.png"></img>)
+    }
+ }
+
+
+
+function List(props){
+
+    return (
+        <ul>
+          {props.items.map(item => <li className={item.done ? "done item" : "item"} key={item.id}>{item.text}
+          <button onClick={()=>{props.onDone(item)}}><DoneImg done={item.done}></DoneImg></button>
+          <button  onClick={()=>{props.onItemDeleted(item)}}><img alt="delete" src="./bin-1.png"></img></button>
+          </li>)}
+        </ul>) 
+}
+
+export default List
